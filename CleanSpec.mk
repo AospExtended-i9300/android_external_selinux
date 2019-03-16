60ef3dae641e043039409725e09279033a72891b
Shilin Victor <chrono.monochrome@gmail.com>
2019-03-16 21:15:43 +0300

    Revert "Mark libselinux as VNDK in Android.bp"
    This reverts commit 122c68c8625e2ae6e386d8b6396088f7bd00be4c.
    
    Change-Id: Ib40c5d5a5fc38f5314c20dbb1500de511880fd9d

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -3,4 +3,3 @@
 #
 $(call add-clean-step, rm -rf $(OUT_DIR)/host/linux-x86/bin/audit2allow)
 $(call add-clean-step, rm -rf $(OUT_DIR)/host/linux-x86/bin/audit2why)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor/lib/libselinux.so)
